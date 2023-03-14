752d3fabc1de951ddd99b121cd1a3a67cba75e46
d.kulish <d.kulish@tg.dunice.net>
2023-03-14 17:48:50 +0300

    cors for requests

--- news/settings.py
+++ news/settings.py
@@ -22,10 +22,8 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5500",
-    "http://127.0.0.1:5500",
-    "http://localhost:3001",
-    "http://127.0.0.1:3001",
+    "http://localhost:8080",
+    "http://192.168.1.166:8080",
     "https://t3.academy.dunice-testing.com",
 ]
 
