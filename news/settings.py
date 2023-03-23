c97e1e9a92931d03cfc0ea633f14c4b21716f4ff
d.kulish <d.kulish@tg.dunice.net>
2023-03-23 15:31:03 +0300

    microchange

--- news/settings.py
+++ news/settings.py
@@ -107,7 +107,8 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
    'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
-   'AUTH_HEADER_TYPES': ('Bearer',),
+   'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    # 'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
 
