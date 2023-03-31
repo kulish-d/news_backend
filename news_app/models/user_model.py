1f5ff1c20f48ffff5dafd9371c650547112b6a33
dmitriy <kulish.dima5410@gmail.com>
2023-03-31 23:38:46 +0300

    micro change for users

--- news_app/models/user_model.py
+++ news_app/models/user_model.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import  AbstractUser
+from django.contrib.auth.models import AbstractUser
 
 from news_app.models.managers import CustomUserManager
 
--- news_app/serializers/user_serializer.py
+++ news_app/serializers/user_serializer.py
@@ -3,16 +3,14 @@ from rest_framework import serializers
 from news_app.models import User
 
 
-
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
         fields = ('id', 'email', 'username', 'password')
 
     def create(self, validated_data):
-        user = User.objects.create(email=validated_data['email'],
-                                    username=validated_data['username']
-                                    )
-        user.set_password(validated_data['password'])
+        user = User.objects.create(**validated_data)
         user.save()
         return user
