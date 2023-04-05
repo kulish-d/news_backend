633b3614bb021d840cdc7fd6e2914d52818f57a2
d.kulish <d.kulish@tg.dunice.net>
2023-04-05 11:12:44 +0300

    now it works, formdata with tags!

--- news_app/views/post_view.py
+++ news_app/views/post_view.py
@@ -3,6 +3,8 @@ from rest_framework import viewsets
 from rest_framework import status
 from rest_framework.response import Response
 
+import json
+
 from news_app.models import Post
 from news_app.serializers import PostSerializer
 from news.settings import NEWS_ON_PAGE
@@ -14,15 +16,15 @@ class PostViewSet(viewsets.ModelViewSet):
 
     def create(self, request):
         good_data = request.data.dict()
-        print(good_data)
-        good_data['tags'] = list(good_data['tags'])
+        good_data['tags'] = json.loads(good_data['tags'])
+        print(good_data['tags'])
+        good_data['tags'] = list(map(lambda tag: {'text': tag}, good_data['tags']))
         print(good_data)
         serializer = self.get_serializer(data=good_data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-        
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
