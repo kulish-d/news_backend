6060cd2257096bdf8b9665a8c2c8e6a2074ca6c1
d.kulish <d.kulish@tg.dunice.net>
2023-04-21 12:02:59 +0300

    create fix

--- news_app/views/comment_view.py
+++ news_app/views/comment_view.py
@@ -3,7 +3,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from news_app.models import Comment
+from news_app.models import Comment, User
 from news_app.serializers import CommentSerializer
 
 
@@ -11,10 +11,12 @@ class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
-    # def create(self, request):
-
-    #     serializer = self.get_serializer(data={})
-
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(author=User.objects.get(id=request.data['author'])) 
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 class CommentView(APIView):
     def get(self, request):
